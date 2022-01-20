ee59946141b7997442b23c95d414f037f04e9006
pedro-ddias <69222299+pedro-ddias@users.noreply.github.com>
2022-01-20 10:54:58 -0300

    Operadores Atribuição

---
+++ Fundamentos/Fundamentos/operadores/operadoresAtribuicao.java
@@ -0,0 +1,11 @@
+package Fundamentos.Fundamentos.operadores;
+
+public class operadoresAtribuicao {
+    public static void main(String[] args) {
+
+        int a = 97; // ele atribui um valor a variavel
+        int b = a; // como a já foi atribuido a algo, isso se torna válido
+        int c = a + b; // atribui o c já com outra operação das outras duas variaveis que só é possivel
+                       // porque ja foram declaradas
+    }
+}
--- Fundamentos/Fundamentos/operadores/operadoresRelacionais.java
+++ Fundamentos/Fundamentos/operadores/operadoresRelacionais.java
@@ -3,25 +3,26 @@ package Fundamentos.Fundamentos.operadores;
 public class operadoresRelacionais {
     public static void main(String[] args) {
 
-        //todo operador relacional tem como padrão voltar verdadeiro ou falso´
+        // todo operador relacional tem como padrão voltar verdadeiro ou falso´
 
-        int a = 12; //isso você está atribuindo um valor a uma variável
+        int a = 12; // isso você está atribuindo um valor a uma variável
         int b = 12;
 
-        System.out.println(a == b); //aqui você pergunta se é igual, uma operação de igualdade
-        /** Temos tbm:
-         * >    maior
-         * >=   maior igual
-         * <    menor
-         * <=   menor igual
-         * !=   diferente
-         * ==   igual a
+        System.out.println(a == b); // aqui você pergunta se é igual, uma operação de igualdade
+        /**
+         * Temos tbm:
+         * > maior
+         * >= maior igual
+         * < menor
+         * <= menor igual
+         * != diferente
+         * == igual a
          */
 
-         double notaAluno = 7.0;
+        double notaAluno = 7.0;
         boolean bomComportamento = true;
 
-        boolean temDesconto = bomComportamento && notaAluno >=7;
-        System.out.println("Tem desconto: " +temDesconto);
+        boolean temDesconto = bomComportamento && notaAluno >= 7;
+        System.out.println("Tem desconto: " + temDesconto);
     }
 }
