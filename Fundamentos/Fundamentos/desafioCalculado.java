9ed5551eb597a65969a5ac8a38d5122eed539c40
pedro-ddias <69222299+pedro-ddias@users.noreply.github.com>
2022-02-08 08:20:55 -0300

    Desafio Final Módulo

---
+++ Fundamentos/Fundamentos/desafioCalculado.java
@@ -0,0 +1,37 @@
+package Fundamentos.Fundamentos;
+
+import java.util.Scanner;
+
+public class desafioCalculado {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Qual o primeiro número?");
+        double num1 = ler.nextDouble();
+        System.out.println("Qual o segundo número?");
+        double num2 = ler.nextDouble();
+
+        System.out.println("Digite o símbolo referente a operação que deseja: + | - | * | / | %");
+        String op = ler.next();
+
+        double resultado;
+        if ("+".equals(op)) {
+            resultado = num1 + num2;
+        }
+        else if ("-".equals(op)) {
+            resultado = num1 - num2;
+        }
+        else if ("*".equals(op)) {
+            resultado = num1 * num2;
+        }
+        else if ("/".equals(op)) {
+            resultado = num1 / num2;
+        } else {
+            resultado = num1 % num2;
+        }
+
+        System.out.println("Seu resultado é: " + resultado);
+        ler.close();
+    }
+}
