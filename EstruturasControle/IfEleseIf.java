0cdda46428adee83b1c243b162f1bfac55920a79
pedro-ddias <69222299+pedro-ddias@users.noreply.github.com>
2022-02-11 09:18:24 -0300

    IfElse

---
+++ EstruturasControle/IfEleseIf.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class IfEleseIf {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+        
+        
+        ler.close();
+    }
+}
---
+++ EstruturasControle/ifElse.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class ifElse {
+
+    public static void main(String[] args) {
+
+        String valor = JOptionPane.showInputDialog("Informe o número");
+        int numero = Integer.parseInt(valor);
+
+        if (numero % 2 == 0) {
+            System.out.println("Número par");
+        } else {
+            System.out.println("Número impar");
+        }
+    }
+}
