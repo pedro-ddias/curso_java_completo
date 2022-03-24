2b0fb4e3cd657fdf3b54875b1a010c86d494a389
pedro-ddias <69222299+pedro-ddias@users.noreply.github.com>
2022-03-24 14:38:48 -0300

    Dasafio dia da semana
    Colocando em prática o if e else if
    Com comparações pelo equals

---
+++ EstruturasControle/DesafioDiaSemana.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class DesafioDiaSemana {
+    public static void main(String[] args) {
+        // domingo = 1
+        // quarta = 4
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite o nome de um dia da semana");
+        String dia = ler.nextLine();
+
+        if (dia.equalsIgnoreCase("domingo")) {
+            System.out.println(1);
+        } else if (dia.equalsIgnoreCase("segunda")) {
+            System.out.println(2);
+        } else if (dia.equalsIgnoreCase("terça")) {
+            System.out.println(3);
+        } else if (dia.equalsIgnoreCase("quarta")) {
+            System.out.println(4);
+        } else if (dia.equalsIgnoreCase("quinta")) {
+            System.out.println(5);
+        } else if (dia.equalsIgnoreCase("sexta")) {
+            System.out.println(6);
+        } else if (dia.equalsIgnoreCase("sábado")) {
+            System.out.println(7);
+        } else {
+            System.out.println("Dia inválido");
+        }
+
+    }
+
+}
