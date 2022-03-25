a2a92aa4f31a52bc570c10a44c0864cfb5d80cf9
pedro-ddias <69222299+pedro-ddias@users.noreply.github.com>
2022-03-25 09:24:16 -0300

    Create WhileDeterminado.java

---
+++ EstruturasControle/WhileDeterminado.java
@@ -0,0 +1,13 @@
+public class WhileDeterminado {
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        // ele é determinador pq foi colocado quantas vezes ee vai repetir
+        // repetir o bom dia 10 vezes
+        while (i <= 10) {
+            System.out.println("Bom dia");
+            i++;
+        }
+    }
+}
